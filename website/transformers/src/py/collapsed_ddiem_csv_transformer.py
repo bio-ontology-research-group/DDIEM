29e2a1eb40c83f3f2eb2ce81cd283be97d3c9a78
ali <ali_raza995@hotmail.com>
2019-11-25 09:39:30 +0300

    fixed google scholar urls

--- website/transformers/src/py/collapsed_ddiem_csv_transformer.py
+++ website/transformers/src/py/collapsed_ddiem_csv_transformer.py
@@ -333,7 +333,7 @@ if __name__ == '__main__':
                             procedure_dict[encrypt_string(procedure_id)].add(OBO.RO_0002558, evidence)
         
                 for referenceStr in row[40].split(","):
-                    referenceStr = referenceStr[0:referenceStr.index('?')] if referenceStr.find('?') > -1 else referenceStr
+                    referenceStr = referenceStr[0:referenceStr.index('?')] if referenceStr.find('?') > -1 and 'scholar.google.com' not in referenceStr else referenceStr
                     referenceStr = referenceStr[0:referenceStr.rindex('/')] if referenceStr.rfind('/') > -1 and referenceStr.rfind('/') == len(referenceStr) - 1 else referenceStr
                     referenceLiteral = Literal(referenceStr.strip()) if referenceStr.strip() else None
                     if referenceLiteral and referenceLiteral not in store.objects(procedure_dict[encrypt_string(procedure_id)], DC.provenance) :
